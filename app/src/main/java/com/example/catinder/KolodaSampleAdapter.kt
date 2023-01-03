abe517529121c86d3e828b6c4434fdab0220f0f7
Sad_Smiley <danilvovikov@gmail.com>
2023-01-03 19:16:40 +0800

    Переписал вьюшку

---
+++ app/src/main/java/com/example/catinder/KolodaSampleAdapter.kt
@@ -0,0 +1,74 @@
+package com.example.catinder
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.squareup.picasso.Picasso
+
+class KolodaSampleAdapter(val context: Context, val data: List<String>?) : BaseAdapter() {
+    private val dataList = mutableListOf<String>()
+
+    init {
+        if (data != null) {
+            dataList.addAll(data)
+        }
+    }
+
+    override fun getCount(): Int {
+        return dataList.size
+    }
+
+    override fun getItem(position: Int): String {
+        return dataList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    fun setData(data: List<String>) {
+        dataList.clear()
+        dataList.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
+        val holder: DataViewHolder
+        var view = convertView
+
+        if (view == null) {
+            view = LayoutInflater.from(parent.context).inflate(R.layout.item_koloda, parent, false)
+            holder = DataViewHolder(view)
+            view?.tag = holder
+
+        } else {
+            holder = view.tag as DataViewHolder
+        }
+
+        holder.bindData(getItem(position))
+        println("check position ${position}")
+
+        return view
+    }
+
+    /**
+     * Static view items holder
+     */
+    class DataViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var picture = view.findViewById<ImageView>(R.id.catImg)
+
+        internal fun bindData( data: String) {
+            Picasso.get().load(data).into(picture);
+            println("data check ${data}")
+        }
+
+    }
+}
--- app/src/main/java/com/example/catinder/MainActivity.kt
+++ app/src/main/java/com/example/catinder/MainActivity.kt
@@ -1,21 +1,98 @@
 package com.example.catinder
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.yalantis.library.Koloda
+import com.yalantis.library.KolodaListener
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var swipeAdapter: SwipeAdapter
-    private lateinit var list: List<Integer>
     private lateinit var koloda: Koloda
+    private lateinit var dislike: ImageView
+    private lateinit var like: ImageView
+    private var adapter: KolodaSampleAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        koloda = findViewById(R.id.koloda)
-
-        swipeAdapter = SwipeAdapter(this, list)
-        koloda.adapter = adapter
+        initElement()
+        initializeDeck()
+        fillData()
+        setUpCLickListeners()
     }
+
+    private fun initElement() {
+        koloda = findViewById(R.id.koloda)
+        dislike = findViewById(R.id.dislike)
+        like = findViewById(R.id.like)
+    }
+
+    private fun initializeDeck() {
+        koloda.kolodaListener = object : KolodaListener {
+
+            internal var cardsSwiped = 0
+
+            override fun onNewTopCard(position: Int) {
+                //todo realize your logic
+            }
+
+            override fun onCardSwipedLeft(position: Int) {
+                Toast.makeText(this@MainActivity, "Left", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCardSwipedRight(position: Int) {
+                Toast.makeText(this@MainActivity, "Right", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onEmptyDeck() {
+                //todo realize your logic
+            }
+        }
+    }
+
+    private fun fillData() {
+        val data = ArrayList<String>()
+        data.add("https://kupit-kota.ru/wp-content/uploads/2022/07/kak-uhajivat-za-kotenkom-2048x1536.jpeg")
+        data.add("https://pic.rutubelist.ru/video/17/b1/17b100a0bcbc6e5e8d11101cde21aca7.jpg")
+        data.add("https://avatars.mds.yandex.net/i?id=501eb8a6f4c7a712a7cac75606ce17be-4230996-images-thumbs&ref=rim&n=33&w=150&h=150")
+        data.add("https://i.pinimg.com/736x/bc/07/7d/bc077d3bee3ed22557493f47378a3f83.jpg")
+        adapter = KolodaSampleAdapter(this, data)
+        koloda.adapter = adapter
+        koloda.isNeedCircleLoading = true
+    }
+
+    private fun setUpCLickListeners() {
+        dislike.setOnClickListener {
+            koloda.onClickLeft()
+            Toast.makeText(this@MainActivity, "Left", Toast.LENGTH_SHORT).show()
+        }
+        like.setOnClickListener {
+            koloda.onClickRight()
+            Toast.makeText(this@MainActivity, "Right", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean { //Подключаем меню к action bar
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_reload, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) { //Слушаем нажатие на кнопку по id
+        R.id.actionReload -> {
+            koloda.reloadAdapterData()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
 }
